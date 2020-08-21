b5c45b88b3c6e1442dbe7a8da6eefdcaa62a4cc5
Adam Madueno <amadueno17@yahoo.com>
2020-08-21 12:56:09 -0700

    added to files

---
+++ routes/itemroutes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router()
+const {  join } = require('path')
+const db = require('../db')
+
+router.get('items', (req, res) => {
+    
+
+})
+
+router.post('./items', (req, res) => {
+
+})
+
+router.put('./items', (req, res) => {
+
+})
+
+router.delete('/items/:id', (req, res) => {
+
+})
+
+
+module.exports = router
---
+++ sql/seed.sql
@@ -0,0 +1,8 @@
+USE todo_db;
+
+INSERT INTO items(text,is_done)
+VALUES('Do laundry', false), 
+('Clean room', false), 
+('Walk dog', false); 
+('Pick up dry cleaning', false)
+('Mow the lawn'), false);
